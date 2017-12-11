452873d835acb79622994f7dc78c4ee0e049d486
mwinb <mwinberry0101@gmail.com>
2017-12-11 14:45:32 -0800

    Code clean up, converted code to hex for readability and input effeciency

--- funcChecker.js
+++ funcChecker.js
@@ -3,110 +3,116 @@
 $(document).ready( function () {
 
 	var deviceList = [];
-	var deviceCodes = [];
 	var currentAccessories = [];
 	var deviceIndex;
 	var previousIndex;
 
-	$('#buttonNewTv').click(function () {
+	$('#buttonNewTv').click(function () 
+	{
 		var tvAccessories = ["Factory Box", 'Hub', "Remote", "Documentation","AC Adapter", "Stands", 
 		"Screws", "HDMI", "Optical", "AV Cable", 'Func Check Done']
-		var tvButtons = [];
 
-		initilizeDevice("TV", tvAccessories, tvButtons);
+		initilizeDevice("TV", tvAccessories);
 
 	});
 
-	$('#buttonNewSoundBar').click(function () {
+	$('#buttonNewSoundBar').click(function () 
+	{
 		var sbAccessories = ["Factory Box", "Documentation","AC Adapter", "Remote",
 		"HDMI", "Optical", "AV Cable", "Aux Cable", 'Func Check Done']
-		var sbButtons = [];
 
-		initilizeDevice("Sound Bar", sbAccessories, sbButtons);
+		initilizeDevice("Sound Bar", sbAccessories);
 
 	});
 
-	$('#buttonMediaPlayer').click(function () {
+	$('#buttonMediaPlayer').click(function () 
+	{
 		var mpAccessories = ['Factory Box','Documentation', 'Remote', 'Documentation', 'AC Adapter',
 		'HDMI', 'Func Check Done'];
-		var mpButtons = [];
 
-		initilizeDevice("Media Player",mpAccessories, mpButtons);
-	})
+		initilizeDevice("Media Player",mpAccessories);
+	});
 
-	$('#buttonHeadPhones').click(function () {
+	$('#buttonHeadPhones').click(function () 
+	{
 		var hpAccessories = ['Factory Box', 'Remote', 'Spare Ear Buds', 'Stand', 'AC adapter',
 		'Documentation', 'Func Check Done']
 
-		var hpButtons = [];
-
-		initilizeDevice("Head Phones", hpAccessories, hpButtons);
-	})
+		initilizeDevice("Head Phones", hpAccessories);
+	});
 
 	$('#buttonReciever').click(function () {
 		var rcAccessories = ['Factory Box', 'Documentation','Remote', 'AV Cables', 'Aux Cable',
 		'Optical Cable', 'HDMI', 'Speaker Cables', 'Func Check Done'];
-		var rcButtons = [];
 
-		initilizeDevice('Reciever', rcAccessories, rcButtons);
-	})
+		initilizeDevice('Reciever', rcAccessories);
+	});
 
 	$('#buttonComputer').click(function () {
 		var pcAccessories = ['Factory Box', 'Documentation', 'Mouse', 'Keyboard',
 		'Pen', 'AC Adaper', 'Speaker/External Device', 'Func Check Done'];
-		var pcButtons = [];
 
-		initilizeDevice("Computer", pcAccessories, pcButtons);
-	})
+		initilizeDevice("Computer", pcAccessories);
+	});
 
-	$('#buttonTablet').click(function () {
+	$('#buttonTablet').click(function () 
+	{
 		var tbAccessories = ['Factory Box', 'Documentation', 'AC power Cord',
 		'USB Block', 'Pen', 'Func Check Done'];
-		var tbButtons = [];
 
-		initilizeDevice("Tablet", tbAccessories, tbButtons);
-	})
+		initilizeDevice("Tablet", tbAccessories);
+	});
 
-	$('#buttonMisc').click(function () {
+	$('#buttonMisc').click(function () 
+	{
 		var mcAccesories = ['Factory Box', 'Charger', 'Remote', 'Documentation',
 		'Hub', 'HDMI', 'Pen', 'Speakers', 'SubWoofer', 'AV Cables', 'Aux Cable', 
 		'Optical Cable', 'SD Card', 'Hardware', 'Stands', 'Func Check Done'];
-		var mcButtons = [];
 
-		initilizeDevice("Miscellaneous Device", mcAccesories, mcButtons);
-	})
+		initilizeDevice("Miscellaneous Device", mcAccesories);
+	});
 
-	var initilizeDevice = function(deviceType,accessories, buttons) {
-		for (var i=0; i < accessories.length; i++) {
+	var initilizeDevice = function(deviceType,accessories) 
+	{
+		var buttons = [];
+		for (var i=0; i < accessories.length; i++) 
+		{
 			buttons.push(newAccessoryButton(accessories[i], i));
 		}
 
 		var deviceBrand = prompt("Enter Device Brand", "Brand");
 		var deviceModel = prompt("Enter Device Model", "Model#");
 
-		if(deviceIndex == null) {
+		if(deviceIndex == null) 
+		{
 			deviceIndex = 0;
 			var newDV =  newDevice(deviceType, deviceIndex, deviceBrand, deviceModel, accessories, buttons);
 			deviceList.push(newDV);
-			addButtons(deviceIndex);
 			
 			previousIndex = deviceIndex;
 			var newCode = deviceList[deviceIndex].getCode();
-			clearAccessoryButtons(deviceIndex);
+
 			deviceList[deviceIndex].setNewButtons();
 			deviceList[deviceIndex].setCode(newCode);
+
 			$(deviceList[deviceIndex].deviceButton.css('background-color', 'coral'));
 		}
-		else {
+		else 
+		{
 			previousIndex = deviceIndex;
-			deviceIndex = deviceIndex + 1;
-			var newTV =  newDevice(deviceType, deviceIndex, deviceBrand, deviceModel, accessories, buttons);
 			
-			deviceList.push(newTV);
+			deviceIndex = deviceIndex + 1;
+			
+			var newDV =  newDevice(deviceType, deviceIndex, deviceBrand, deviceModel, accessories, buttons);
+			
+			deviceList.push(newDV);
+			
 			clearAccessoryButtons(previousIndex);
-			addButtons(deviceIndex)
+
 			var newCode = deviceList[deviceIndex].getCode();
+
 			clearAccessoryButtons(deviceIndex);
+
 			deviceList[deviceIndex].setNewButtons();
 			deviceList[deviceIndex].setCode(newCode);
 			
@@ -115,42 +121,59 @@ $(document).ready( function () {
 		}
 	}
 
-	$('#buttonCode').click(function () {
-		if($('#oldCode').val() != '' && deviceIndex != null) {
-			try {
+	$('#buttonCode').click(function () 
+	{
+		if( $('#oldCode').val() != '' && deviceIndex != null ) 
+		{
+			try 
+			{
 				var oldCode = $('#oldCode').val();
+				oldCode = parseInt(oldCode, 16).toString(2);
 				var currentDevice = deviceList[deviceIndex];
+				currentDevice.setNewButtons();
 				currentDevice.setCode(oldCode);	
 			}
-			catch (err) {
+			catch (err) 
+			{
 				alert("Improper Code. Make Sure Device Selected is Correct Type");
 			}
 		}
-		else {
+		else 
+		{
 			alert("Error. Make Sure Code Entered is Valid");
 		}
 	})
 
 
-	var clearAccessoryButtons = function (prevIndex) {
-		for (var i = 0; i < deviceList[prevIndex].accessoryButtons.length; i++) {
+	var clearAccessoryButtons = function (prevIndex) 
+	{
+		for (var i = 0; i < deviceList[prevIndex].accessoryButtons.length; i++) 
+		{
 			$('#' + deviceList[prevIndex].accessoryButtons[i].attr('id')).remove();
 		}
 	}
 
-	var addButtons = function(currentIndex) {
+	var addButtons = function(currentIndex) 
+	{
 		var tempDevice = deviceList[currentIndex];
-		for (var i = 0; i < deviceList[currentIndex].accessoryButtons.length; i++) {
+		for (var i = 0; i < deviceList[currentIndex].accessoryButtons.length; i++) 
+		{
 			var currentButton = deviceList[currentIndex].accessoryButtons[i];
+			
 			$('#devicesButtons').append(currentButton);
-			if(currentButton.attr('clicked') == 'true') {
+			
+			if(currentButton.attr('clicked') == 'true') 
+			{
 				currentButton.css('background-color', 'coral');
-			} else {
+			} 
+			else 
+			{
 				currentButton.css('background-color', 'slategrey');
 			}
 		}
 
-		for (var i =0; i < deviceList.length; i++) {
+		for (var i =0; i < deviceList.length; i++) 
+		{
 			$('#devices').append(deviceList[i].deviceButton);
 		}
 
@@ -159,20 +182,24 @@ $(document).ready( function () {
 		$('#brandTitle').html(deviceBrand);
 		$('#modelTitle').html(deviceList[currentIndex].model);
 		var newCode = tempDevice.code;
-		$('#currentCode').html(newCode);
+		var hexCode = parseInt(newCode, 2).toString(16).toUpperCase();
+		$('#currentCode').html(hexCode);
 
 	}
 
 
-	var newDeviceButton = function(deviceType, Index, deviceName, deviceModel) {
-		var $btn = $('<button/>', {
+	var newDeviceButton = function(deviceType, Index, deviceName, deviceModel) 
+	{
+		var $btn = $('<button/>', 
+		{
 			id: deviceType + Index,
 			class: 'buttons'
 		});
 
 		$($btn).html(deviceType + " " + deviceName + " " + deviceModel + " " + Index);
 
-		$($btn).on('click', function () {
+		$($btn).on('click', function () 
+		{
 			previousIndex = deviceIndex;
 			deviceIndex = Index;
 			$('#brandTitle').html(deviceName);
@@ -184,13 +211,15 @@ $(document).ready( function () {
 			$(deviceList[deviceIndex].deviceButton.css('background-color', 'coral'));
 			$(deviceList[previousIndex].deviceButton.css('background-color', 'slategrey'));
 
-		})
+		});
 
 		return $btn
 	}
 
-	var newAccessoryButton = function (buttonValue, buttonIndex) {
-		var $btn = $('<button/>', {
+	var newAccessoryButton = function (buttonValue, buttonIndex) 
+	{
+		var $btn = $('<button/>', 
+		{
 			id: 'accessory' + buttonIndex,
 			type: 'button',
 			value: buttonValue,
@@ -201,13 +230,16 @@ $(document).ready( function () {
 
 		$($btn).html(buttonValue);
 
-		$($btn).on('click', function () {
-			if ($($btn).attr('clicked') == 'false') {
+		$($btn).on('click', function () 
+		{
+			if ($($btn).attr('clicked') == 'false') 
+			{
 				$($btn).attr('clicked', 'true');
 				$($btn).css('background-color', 'coral');
 				deviceList[deviceIndex].code = deviceList[deviceIndex].getCode();
 			}
-			else if ($($btn).attr('clicked') == 'true') {
+			else if ($($btn).attr('clicked') == 'true') 
+			{
 				$($btn).attr('clicked', 'false');
 				$($btn).css('background-color', 'slategrey');
 				deviceList[deviceIndex].code = deviceList[deviceIndex].getCode();
@@ -215,16 +247,18 @@ $(document).ready( function () {
 
 			var tempDevice = deviceList[deviceIndex];
 			var newCode = tempDevice.getCode();
-			$('#currentCode').html(newCode);
+			var hexCode = parseInt(newCode, 2).toString(16).toUpperCase();
+			$('#currentCode').html(hexCode);
 			
-		})
+		});
 
 		return $btn
 	}
 
 
 	var newDevice = function (deviceType, deviceIndex,
-		deviceName, deviceModel, deviceAccessories, deviceButtons) {
+		deviceName, deviceModel, deviceAccessories, deviceButtons) 
+	{
 		return {
 			type: deviceType,
 
@@ -246,10 +280,12 @@ $(document).ready( function () {
 			getCode: function () {
 				var newCode = "";
 				for (var i = 0; i < this.accessoryButtons.length; ++i) {
-					if (this.accessoryButtons[i].attr('clicked') == 'false') {
+					if (this.accessoryButtons[i].attr('clicked') == 'false') 
+					{
 						newCode += 0;
 					}
-					else {
+					else 
+					{
 						newCode += 1;
 					}
 
@@ -258,13 +294,17 @@ $(document).ready( function () {
 				return newCode;
 			},
 
-			setCode: function (inputedCode) {
+			setCode: function (inputedCode) 
+			{
 				var newCode = inputedCode;
-				for (var i = 0; i < this.accessoryButtons.length; ++i) {
-					if(newCode.charAt(i) == '1') {
+				for (var i = 0; i < this.accessoryButtons.length; ++i) 
+				{
+					if(newCode.charAt(i) == '1') 
+					{
 						this.accessoryButtons[i].attr('clicked', 'true');
 					}
-					else if (newCode.charAt(i)=='0'){
+					else if (newCode.charAt(i)=='0')
+					{
 						this.accessoryButtons[i].attr('clicked', 'false');
 					}
 				}
@@ -277,9 +317,11 @@ $(document).ready( function () {
 
 			},
 
-			setNewButtons: function() {
+			setNewButtons: function() 
+			{
 				this.accessoryButtons = [];
-				for (var i = 0; i < this.accessories.length; i++) {
+				for (var i = 0; i < this.accessories.length; i++) 
+				{
 					this.accessoryButtons.push(newAccessoryButton(this.accessories[i]));
 				}
 
